5a6c52f648cfd1d2a371d34151ac7fa9617ef27b
Sergei Kopytov <kot63727@gmail.com>
2024-02-15 00:05:54 +0500

    add examples data

--- src/app/[locale]/examples/page.tsx
+++ src/app/[locale]/examples/page.tsx
@@ -1,10 +1,15 @@
 import MainTitle from "@/components/Common/Titles/MainTitle/MainTitle";
 import classes from "./page.module.css";
+import { projectsList } from "@/data/examples";
+import ProjectView from "@/components/Common/Views/ProjectView/ProjectView";
 
 export default function Examples() {
   return (
     <div className={classes.container}>
       <MainTitle title="Examples" />
+      {projectsList.map((project) => {
+        return <ProjectView key={project.name} {...project} />;
+      })}
     </div>
   );
 }
